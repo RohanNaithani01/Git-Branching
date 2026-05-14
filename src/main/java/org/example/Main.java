e52cc2e197292c6a2c7e7ebf9e96fcaef24b96af
RohanNaithani01 <rohannaithani02@gmail.com>
2026-05-14 10:36:43 +0530

    New Feature

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,7 @@
 package org.example;
 public class Main {
     static void main() {
-        //System.out.println("This is Version 1");
-        //System.out.println("Now I comment Version 1 & add Version 2");
-        //System.out.println("Now I comment version 1 & 2 and add Version 3");
-        //System.out.printf("This is Feature 1");
-        //System.out.println("This is Feature 2");
         System.out.println("Feature Done");
+        System.out.println("New feature Added");
     }
 }
