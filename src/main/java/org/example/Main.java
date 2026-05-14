7a6659bf6131c2a84499fef0edb984dbe2ef04bb
RohanNaithani01 <rohannaithani02@gmail.com>
2026-05-14 10:18:06 +0530

    feature 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ public class Main {
         //System.out.println("This is Version 1");
         //System.out.println("Now I comment Version 1 & add Version 2");
         //System.out.println("Now I comment version 1 & 2 and add Version 3");
-        System.out.printf("This is Feature 1");
+        //System.out.printf("This is Feature 1");
+        System.out.println("This is Feature 2");
     }
 }
