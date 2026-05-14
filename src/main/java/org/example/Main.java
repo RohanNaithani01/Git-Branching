f9a40ee848cbd0cced6d5ed40aad334ac78f8c71
RohanNaithani01 <rohannaithani02@gmail.com>
2026-05-14 10:08:58 +0530

    Version 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 public class Main {
     static void main() {
-        System.out.println("This is Version 1");
+        //System.out.println("This is Version 1");
+        System.out.println("Now I comment Version 1 & add Version 2");
     }
 }
